de0384db57dae2d8f4da3900434692538cb1e977
Danilashionshion <danilofernandesdasilva29@gmail.com>
2024-10-14 16:21:27 -0300

    Nova atualização

--- Cart.js
+++ Cart.js
@@ -1,12 +1,18 @@
-import { TouchableOpacity } from 'react-native';
+import { TouchableOpacity, TouchableOpacityComponent } from 'react-native';
 import { Text, SafeAreaView, StyleSheet,View } from 'react-native';
 import { ScrollView } from 'react-native-web';
+import { useNavigation } from '@react-navigation/native';
+import Carrinho from './components/carrinho.js';
 
 export default function Cart(){
+
+    const navigation = useNavigation()
+
     return(
         <ScrollView>
         <Text> Hello, World </Text>
-        <TouchableOpacity onPress={() => window.location.href = 'Home'}> Clique aqui para voltar</TouchableOpacity>
+        <TouchableOpacity onPress={() => navigation.navigate('Home')}> Clique aqui para voltar</TouchableOpacity>
+        <Carrinho/>
         </ScrollView>
     )
 }
