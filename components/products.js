d20c1f385d77649b3a1a08e27470a4bc77c3ede1
Danilashionshion <danilofernandesdasilva29@gmail.com>
2024-10-15 17:09:38 -0300

    Another atualization

--- components/products.js
+++ components/products.js
@@ -1,109 +1,77 @@
 import { useEffect, useState } from 'react';
-import { Text, SafeAreaView, StyleSheet, FlatList, Image,View,Pressable} from 'react-native';
+import { Text, SafeAreaView, StyleSheet, FlatList, Image, View, Pressable } from 'react-native';
+import produtos from './produtinhoinho';
+import Carrinho from './carrinho';
 
-export default function Produtos(props){
+export default function Produtos(props) {
 
+    const [categoria, setCategoria] = useState("")
+    const [estadocategoria, setestadoCategoria] = useState(false)
+    const [sorts, setsort] = useState([])
+    const [Carrinho, setCarrinho] = useState([])
 
     
-    const[categoria, setCategoria] = useState("")
-    const[estadocategoria, setestadoCategoria] = useState(false)
-    const[sorts,setsort] = useState([])
-
-    
-    
-    const produtos=[{
-            id: 1,
-            img: require('../assets/-1086821640.jpg'),
-            category: "CellPhone",
-            title: "Zelular top, até roda vscode R$: ",
-            price: 99.9 
-        },
-        {
-            id: 2,
-            img: require('../assets/-1086821640.jpg'),
-            category: "CellPhone",
-            title: "Gelular top, até roda vscode, só que 2 R$: ",
-            price: 109.9 
-        },
-        {
-            id: 3,
-            img: require('../assets/smart.png'),
-            category: "SmartWatch",
-            title: "Esse relogio que brilha e é caro RS$ ",
-            price: 80.0
-        },
-        {
-            id: 4,
-            img: require('../assets/pc.png'),
-            category: "PersonalComputer",
-            title: "PC GAMER, roda java a 60fps",
-            price: 400.0
-        },
-        {   
-            id: 5,
-            img: require('../assets/pc.png'),
-            category: "PersonalComputer",
-            title: "Computador da xuxa, só que 2024 (roda zelda minish cap)",
-            price: 50.0
-        },
-    ]
-    function filtros(categoria){
+    function filtros(categoria) {
         setCategoria(categoria)
         setestadoCategoria(true)
         console.log(categoria)
     }
-    
-    function clear(){
+
+    function clear() {
         setestadoCategoria(false)
         setsort(false)
     }
 
-    
 
-    const  filtro = estadocategoria? produtos.filter((p,i) => p.category == categoria ): produtos 
-     
-
-
+    const filtro = estadocategoria ? produtos.filter((p, i) => p.category == categoria) : produtos
     function handleSort() {
         setsort(true)
     }
-    if(sorts == true){
-       filtro.sort((a,b) => 
-            (a.title > b.title ? 1 :
-            b.title > a.title ? -1 : 0));
-            console.log(filtro)
+
+    function handleAdd(id) {
+        const existingItem = Carrinho.find((i) => i.id === id);
+        const item = produtos.find((p) => p.id === id);
+       setCarrinho([...Carrinho, { ...item }]);
+
+        console.log(Carrinho);
+        
     }
+    if (sorts == true) {
+        filtro.sort((a, b) =>
+        (a.title > b.title ? 1 :
+            b.title > a.title ? -1 : 0));
+        console.log(filtro)
+    }
+    console.log(Carrinho)
+    return (
+        <SafeAreaView>
+            <View style={styles.subheader}>
+                <Pressable style={styles.subheaderparagraph} onPress={() => filtros("CellPhone")}>Celulares</Pressable>
+                <Pressable style={styles.subheaderparagraph} onPress={() => filtros("SmartWatch")}>SmartWatch</Pressable>
+                <Pressable style={styles.subheaderparagraph} onPress={() => filtros("PersonalComputer")}>Computadores</Pressable>
+                <Pressable style={styles.subheaderparagraph} onPress={() => handleSort()}>ordem alfabética</Pressable>
+                <Pressable style={styles.subheaderparagraph} onPress={() => clear()}> Limpar Estado </Pressable>
+            </View>
 
+            <Text>
+                <FlatList
+                    data={filtro}
+                    renderItem={({ item }) => (
+                        <View style={styles.itemContainer} >
+                            <Pressable style={styles.itemContainer} id={item.id} onPress={() => handleAdd(item.id)} >
+                                <Image style={styles.imagem} source={item.img} />
+                                <Text style={styles.paragraph}>{item.title}</Text>
+                                <Text style={styles.price}>R$ {item.price}</Text>
+                            </Pressable>
 
-    return(
-    <SafeAreaView>
-        <View style={styles.subheader}>
-            <Pressable style={styles.subheaderparagraph} onPress={() => filtros("CellPhone")}>Celulares</Pressable>
-            <Pressable style={styles.subheaderparagraph} onPress={() => filtros("SmartWatch")}>SmartWatch</Pressable>
-            <Pressable style={styles.subheaderparagraph} onPress={() => filtros("PersonalComputer")}>Computadores</Pressable>
-            <Pressable style={styles.subheaderparagraph} onPress={() => handleSort()}>ordem alfabética</Pressable>
-            <Pressable style={styles.subheaderparagraph} onPress={() => clear()}> Limpar Estado </Pressable>
-        </View>
+                        </View>
+                    )}
+                    keyExtractor={item => item.id.toString()}
+                    horizontal={false}
+                />
+            </Text>
+        </SafeAreaView>
 
-        <Text>
-            <FlatList
-                data={filtro}
-                renderItem={({ item }) => (
-                    <View style={styles.itemContainer}>
-                        <Pressable style={styles.itemContainer}>
-                            <Image style={styles.imagem} source={item.img}/>
-                            <Text style={styles.paragraph}>{item.title}</Text>
-                            <Text style={styles.price}>R$ {item.price}</Text> 
-                        </Pressable>
-
-                    </View>
-                )}
-                keyExtractor={item => item.id.toString()}
-                horizontal={true}
-            />
-        </Text>
-    </SafeAreaView>
-   
     )
 }
 
@@ -115,6 +83,10 @@ const styles = StyleSheet.create({
     itemContainer: {
         alignItems: 'center',
         marginRight: 10,
+        padding: 10,
+        borderWidth: 1,
+        borderColor: '#ccc',
+        borderRadius: 5,
     },
     imagem: {
         width: 100,
@@ -129,11 +101,15 @@ const styles = StyleSheet.create({
         color: 'green',
     },
     subheader: {
-        marginTop: 200,
-        justifyContent: 'center',
+        marginTop: 20,
         flexDirection: 'row',
+        justifyContent: 'space-around',
     },
     subheaderparagraph: {
-        marginRight: 10
-    }
-})
+        fontSize: 16,
+        fontWeight: 'bold',
+        padding: 10,
+    },
+});
+
+
